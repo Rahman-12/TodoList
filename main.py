f685e02ad2160e4a6858a84714c2833660a5c6cd
Rahman-12 <yusuf.dammy2019@gmail.com>
2025-08-18 18:20:59 +0100

    push chancges

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 import flask 
+from flask import Flask , render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    todos = ['go to college on time', 'sleep early','smile on wake up']
+    return render_template('index.html',todos = todos)
+
+
+
+if __name__ == "__main__":
+    app.run(debug = True)
