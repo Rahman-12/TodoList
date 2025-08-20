1348d1e4aa5089812dac5e8eff7ed650dcda69ad
Rahman-12 <yusuf.dammy2019@gmail.com>
2025-08-20 17:01:12 +0100

    finall

--- main.py
+++ main.py
@@ -1,12 +1,35 @@
 import flask 
-from flask import Flask , render_template
+from flask import Flask , render_template, request, redirect
+import json
 
 app = Flask(__name__)
+def load_todo():
+    with open("database.json") as file:
+        return json.load(file)
+    
+def save_todos(data):
+    with open("database.json","w") as file:
+        json.dump(data,file)
+
+
+
+
 
 @app.route('/')
 def index():
-    todos = ['go to college on time', 'sleep early','smile on wake up','test']
+    todos = load_todo()
     return render_template('index.html',todos = todos)
+
+
+@app.route('/add',methods=['POST',"GET"])
+def add_task():
+    if request.method == "POST":
+        task = request.form["addtodo1"]
+        todos = load_todo()
+        todos.append(task)
+        save_todos(todos)
+        return redirect('/')
+    return "normal site"
 
 
 
